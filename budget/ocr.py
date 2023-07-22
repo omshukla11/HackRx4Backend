826d15670762f7366d6deb0f90eae071c806759c
Om Shukla <shuklaom1208@gmail.com>
2023-07-22 11:01:18 +0530

    CORS bug solved

--- budget/ocr.py
+++ budget/ocr.py
@@ -35,7 +35,7 @@ def get_details(concat_output):
     sent_tokens = nltk.sent_tokenize(concat_output)
     reciever = sent_tokens[0].splitlines()[0]
 
-    price = re.findall(r'Total:\s*(?:₹)?(\d+)', concat_output)
+    price = re.findall(r'Total:*(?:₹)?(\d+)', concat_output)
     price = list(map(float, price))
     price = sorted(price)
     if price:
